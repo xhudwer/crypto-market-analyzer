64df85df9e812f5be8ae568ff295b2eae3b06110
xhudwer <112972731+xhudwer@users.noreply.github.com>
2026-09-05 21:58:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -92,19 +92,10 @@ def health():
     return {"status": "ok"}
 
 
-if __name__ == "__main__":
+@app.on_event("startup")
+def startup_event():
     thread = threading.Thread(
         target=market_monitor,
         daemon=True
     )
     thread.start()
-
-    import uvicorn
-
-    port = int(os.environ.get("PORT", 10000))
-
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=port
-    )
